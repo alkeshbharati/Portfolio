66281ef6b040740815e0f244661fc262b9f8af14
Alkesh Bharati <alkeshbharati@MacBook-Pro-4.local>
2020-02-04 16:43:01 -0600

    Updates

---
+++ precache-manifest.fcdbda9e9a0d7cc9618dd9ec5b928a8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fd38479971f87cddf94c5f06551102",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "af79a36b9ad22f99d67b",
+    "url": "/portfolio/static/css/main.79827d19.chunk.css"
+  },
+  {
+    "revision": "1ce80c1de81f1549dc93",
+    "url": "/portfolio/static/js/2.c4d6152c.chunk.js"
+  },
+  {
+    "revision": "af79a36b9ad22f99d67b",
+    "url": "/portfolio/static/js/main.aa2fdc75.chunk.js"
+  },
+  {
+    "revision": "4234e20402381065f478",
+    "url": "/portfolio/static/js/runtime-main.01d0503f.js"
+  }
+]);
